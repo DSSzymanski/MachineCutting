8abbe5db1afc95001b6d12200ecce7e5b798d03b
DSSzymanski <dszymanski@umass.edu>
2019-08-17 17:10:58 -0400

    added cut class and validator for inputs

---
+++ MachinCuttingApp/Cut.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachinCuttingApp
+{
+    public class Cut
+    {
+        public static readonly int NORTH = 0;
+        public static readonly int SOUTH = 1;
+        public static readonly int EAST = 2;
+        public static readonly int WEST = 3;
+
+        private int startXPos { get; }
+        private int startYPos { get; }
+        private int endXPos { get; set; }
+        private int endYPos { get; set; }
+
+        public Cut(int x1, int y1, int len, int dir)
+        {
+            this.startXPos = x1;
+            this.startYPos = y1;
+            this.setEndPos(len: len, dir: dir);
+        }
+
+        private void setEndPos(int len, int dir)
+        {
+            if (dir == NORTH)
+            {
+                this.endXPos = this.startXPos;
+                this.endYPos = this.startYPos - len;
+            }
+            else if (dir == SOUTH)
+            {
+                this.endXPos = this.startXPos;
+                this.endYPos = this.startYPos + len;
+            }
+            else if (dir == EAST)
+            {
+                this.endXPos = this.startXPos - len;
+                this.endYPos = this.startYPos;
+            }
+            else if (dir == WEST)
+            {
+                this.endXPos = this.startXPos + len;
+                this.endYPos = this.startYPos;
+            }
+
+        }
+    }
+}
---
+++ MachinCuttingApp/Validator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachinCuttingApp
+{
+    static class Validator
+    {
+        /*
+         * Takes a string of input and validates
+         * to ensure input is integer value
+         * and value is positive. Returns
+         * valid parsed number or -1.
+         */
+        static int validateInput(string input)
+        {
+            try
+            {
+                int len = Int32.Parse(input);
+                return len;
+            }
+            catch (FormatException)
+            {
+                return -1;
+            }
+        }
+    }
+}
